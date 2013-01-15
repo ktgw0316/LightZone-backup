d8f5c269462cee2b6efee54f86d933946e69ea3c
KITAGAWA Masahiro <arctica0316@gmail.com>
2013-01-15 23:20:42 +0900

    Added Japanese translation of help/*.html; Set tidy -utf8

--- linux/help/tidyall.sh
+++ linux/help/tidyall.sh
@@ -4,7 +4,7 @@ files=`ls *.html`
 
 for file in $files; do
     echo tidy $file
-    tidy -m -i -q -ashtml --show-warnings false $file
+    tidy -m -i -q -ashtml -utf8 --show-warnings false $file
     if [ $? -eq 2 ]; then
         echo "tidy error"
         exit 1
