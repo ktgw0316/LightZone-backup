bdf2076638f741dd27c41185dace6fb4bfa9e821
KITAGAWA Masahiro <arctica0316@gmail.com>
2020-03-25 23:46:18 +0900

    Gradle - Add :lightcrafts:resources  task

--- lightcrafts/build.gradle.kts
+++ lightcrafts/build.gradle.kts
@@ -1,8 +1,13 @@
+import java.io.FileOutputStream
+
 val MAKE = "make"
 
 dependencies {
-    "implementation"(files("lib/substance-lite.jar"))
-    "implementation"(files("lib/laf-widget-.jar"))
+    "implementation"(files("${projectDir}/lib/substance-lite.jar"))
+    "implementation"(files("${projectDir}/lib/laf-widget-.jar"))
+}
+javafx {
+    modules = listOf("javafx.controls", "javafx.swing")
 }
 tasks {
     register<Exec> ("coprocesses") {
@@ -11,8 +16,40 @@ tasks {
     register<Exec> ("cleanCoprocesses") {
         commandLine(MAKE, "-C", "coprocesses", "-j", "-s", "clean")
     }
+    register<Task> ("revision") {
+        val dir = "${project.buildDir}/resources/main/com/lightcrafts/utils/resources"
+        mkdir(dir)
+        val file = File("$dir/Revision")
+        file.delete()
+        FileOutputStream(file).use {
+            project.exec {
+                commandLine("git", "log", "-1")
+                standardOutput = it
+            }
+            it.toString().trim()
+        }
+        FileOutputStream(file, true).use {
+            project.exec {
+                commandLine("git", "config", "--list")
+                standardOutput = it
+            }
+            it.toString().trim()
+        }
+    }
+    register<Copy> ("version") {
+        from("version.txt")
+        into("${project.buildDir}/resources/main/com/lightcrafts/utils/resources/Version")
+    }
+    register<Copy> ("resources") {
+        dependsOn("revision")
+        dependsOn("version")
+        from("${projectDir}/src/main/resources")
+        from("${projectDir}/src/main/locale")
+        exclude("**/.git")
+        into("${project.buildDir}/main")
+    }
     getByName("build") {
-        dependsOn("coprocesses")
+        dependsOn("coprocesses", "resources")
     }
     getByName("clean") {
         dependsOn("cleanCoprocesses")
